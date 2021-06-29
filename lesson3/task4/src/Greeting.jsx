fbfe358d9e3cf8f360c967a37c44c66f0a23af01
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-29 21:01:09 +0300

    task4

---
+++ lesson3/task4/src/Greeting.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import moment from "moment";
+
+
+const Greeting = props => {
+  const formatDate = date => moment(date).format();
+  const timeString = formatDate(props.birthDate);
+  console.log(timeString);
+  const data = `${timeString.substr(0, 4)}${timeString.substr(5, 2)}${timeString.substr(8, 2)}`;
+  console.log(data);
+  const time = moment(data, "YYYYMMDD").fromNow();
+  console.log(time);
+  const getTime = time => {
+    if (time.includes('years')) {
+       return Number.parseInt(time);
+    }
+    if (time.includes('year')) {
+      return 1;
+   }
+   return 0;
+ }
+  return (
+    <div className="greeting">
+    My name is {props.firstName} {props.lastName}. I'm {getTime(time)} years old
+    </div>
+  );
+}
+
+export default Greeting;
