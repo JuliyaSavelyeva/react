e67b320d1548b63e4be9dca3971e4b9224d09bb6
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-29 18:10:59 +0300

    add

--- lesson3/task1/src/index.js
+++ lesson3/task1/src/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './index.scss';
+import './search.scss';
 
-const renderSeconds = time => {
-  const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector('#root');
 
-  const seconds = new Date(time).getSeconds();
-  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
-  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
-
-  const styles = {
-    color: textColor,
-    backgroundColor,
-  };
-
-  const elem = (
-    <div className="seconds" style={styles}>
-      Now is {seconds}
+const SearchField = props => {
+  console.log(props);
+  const element = (
+    <div className="search">
+      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+      <div className="search__field">
+        <input type="text" className="search__input" />
+        <button className="search__button">Search</button>
+      </div>
     </div>
   );
-
-  ReactDOM.render(elem, rootElement);
+  return element;
 };
 
-setInterval(() => {
-  renderSeconds(new Date());
-}, 1000);
+ReactDOM.render(<SearchField name="Bob" />, rootElement);
--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './index.scss';
+import './search.scss';
 
-const renderSeconds = time => {
-  const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector('#root');
 
-  const seconds = new Date(time).getSeconds();
-  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
-  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+// const SearchField = props => {
+//   console.log(props);
+//   const element = (
+//     <div className="search">
+//       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//       <div className="search__field">
+//         <input type="text" className="search__input" />
+//         <button className="search__button">Search</button>
+//       </div>
+//     </div>
+//   );
+//   return element;
+// };
 
-  const styles = {
-    color: textColor,
-    backgroundColor,
-  };
+class SearchField extends Component {
+  render() {
+    return (
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
+      </div>
+    );
+  }
+}
 
-  const elem = (
-    <div className="seconds" style={styles}>
-      Now is {seconds}
-    </div>
-  );
-
-  ReactDOM.render(elem, rootElement);
-};
-
-setInterval(() => {
-  renderSeconds(new Date());
-}, 1000);
+ReactDOM.render(<SearchField name="Bob" />, rootElement);
