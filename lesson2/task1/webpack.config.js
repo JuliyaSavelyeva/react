267921619913fa8d5da7c833f21262c4bc0a1b54
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-29 09:13:49 +0300

    add lesson2

--- lesson2/task1/webpack.config.js
+++ lesson2/task1/webpack.config.js
@@ -1,12 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
-      {
-        test: /.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
       {
         test: /.js$/,
         use: ['babel-loader'],
@@ -14,6 +16,8 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
