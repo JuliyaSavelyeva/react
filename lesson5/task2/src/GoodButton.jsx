25b277baf2b4731ac64bd07fb673a335d10966d3
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-30 18:27:31 +0300

    add

--- lesson5/task2/src/GoodButton.jsx
+++ lesson5/task2/src/GoodButton.jsx
@@ -4,7 +4,7 @@ class GoodButton extends React.Component {
   handleClick(e) {
     console.log(e.target.textContent);
     console.log(e);
-    alert('Good job!');
+    alert(e.target.textContent);
   }
 
   render() {
