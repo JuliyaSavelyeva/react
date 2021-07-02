1b6ed3302c3393a12e182ec21406f0d073f0ac13
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-07-02 19:22:05 +0300

    add

--- lesson6/task4/src/App.jsx
+++ lesson6/task4/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Status from './Status.jsx';
 
-const App = () => (
-    <Status />
- )
+const App = () => <Status />;
 
 export default App;
--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -3,9 +3,13 @@ import Online from './Online.jsx';
 import Offline from './Offline.jsx';
 
 class Status extends React.Component {
-  state = {
-    isOnline: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOnline: false,
+    };
+  }
 
   handlerReconnect = () => {
     this.setState({
