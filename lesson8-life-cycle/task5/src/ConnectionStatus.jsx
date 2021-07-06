76aa0178967c85b71ac50db6cdfec0e7a5b899e3
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-07-06 17:17:21 +0300

    add

---
+++ lesson8-life-cycle/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+  state = {
+    status: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.onOnlineSatus);
+    window.addEventListener('offline', this.onOfflineSatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.onOnlineSatus);
+    window.removeEventListener('offline', this.onOfflineSatus);
+  }
+
+  onOnlineSatus = () => {
+    this.setState({
+      status: true,
+    });
+  };
+
+  onOfflineSatus = () => {
+    this.setState({
+      status: false,
+    });
+  };
+
+  render() {
+    return this.state.status ? (
+      <div className="status">Online</div>
+    ) : (
+      <div className="status status_offline">Offline</div>
+    );
+  }
+}
+
+export default ConnectionStatus;
